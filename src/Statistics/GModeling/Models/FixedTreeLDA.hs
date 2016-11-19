f6bac03656e91cb5f969772eb0b432f0c9bef82b
Naren Sundaravaradan <narens@solitontech.com>
2016-11-19 23:16:54 +0530

    FixedTreeLDA

---
+++ src/Statistics/GModeling/Models/FixedTreeLDA.hs
@@ -0,0 +1,21 @@
+module Statistics.GModeling.Models.FixedTreeLDA
+  (
+  ) where
+
+import Statistics.GModeling.DSL
+
+data LDATreeLabels =
+    Alpha1 | Alpha2 | Beta
+  | TopicDepth | TopicPath | Topic | Doc
+  | Symbols | Symbol
+
+ldaTree :: Network LDATreeLabels
+ldaTree =
+  [
+    Only Alpha1         :-> TopicDepth
+  , Only Alpha2         :-> TopicPath
+  , Only Beta           :-> Symbols
+  , (TopicPath :@ [Doc])  :-> Topic
+  , (TopicDepth :@ [Doc]) :-> Topic
+  , (Symbols :@ [Topic])  :-> Symbol
+  ]
