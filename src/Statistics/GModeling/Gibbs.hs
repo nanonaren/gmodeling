cb4cf2db4d1ec6eb77f694b76224da447fe5597d
Naren Sundaravaradan <narens@solitontech.com>
2016-11-20 09:59:37 +0530

    Starting a module for Gibbs interpreter

---
+++ src/Statistics/GModeling/Gibbs.hs
@@ -0,0 +1,23 @@
+module Statistics.GModeling.Gibbs
+  (
+    Reader (..)
+  , Writer (..)
+  ) where
+
+data Reader k v = Reader
+  {
+    -- | Number of available indices
+    size :: Int
+    -- | Read the value at the given key
+  , read :: Int -> k -> v
+    -- | Create a copy for writing only
+  , copy :: IO (Writer k v)
+  }
+
+data Writer k v = Writer
+  {
+    -- | Write the value at the given key
+    write :: Int -> k -> v -> IO ()
+    -- | Create a read-only copy
+  , readOnly :: IO (Reader k v)
+  }
